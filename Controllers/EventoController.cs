a0d239ac087a619c29734360b0b93fad1d303bac
Joelton <114880776+joeltonken@users.noreply.github.com>
2024-04-30 23:41:08 -0300

    :sparkles: feat: Adiciona CRUD

--- Controllers/EventoController.cs
+++ Controllers/EventoController.cs
@@ -18,5 +18,89 @@ namespace EventoApplication.Controllers
             IEnumerable<EventosModel> eventos = _db.Eventos;
             return View(eventos);
         }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            EventosModel eventos = _db.Eventos.FirstOrDefault(x => x.Id == id);
+
+            if (eventos == null) 
+            {
+                return NotFound();
+            }
+
+            return View(eventos);
+        }
+
+        [HttpGet]
+        public IActionResult Excluir(int? id) 
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            EventosModel evento = _db.Eventos.FirstOrDefault(x => x.Id == id);
+
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            return View(evento);
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(EventosModel eventos)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Eventos.Add(eventos);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Editar(EventosModel evento)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _db.Eventos.Update(evento);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(evento);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir(EventosModel evento)
+        {
+            if(evento == null)
+            {
+                return NotFound();
+            }
+            _db.Eventos.Remove(evento);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
     }
 }
--- Models/EventosModel.cs
+++ Models/EventosModel.cs
@@ -1,11 +1,20 @@
-﻿namespace EventoApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventoApplication.Models
 {
     public class EventosModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Contratante!")]
         public string Contratante { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome da Empresa!")]
         public string Empresa { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome do Evento!")]
         public string Evento { get; set; }
+
         public DateTime dataUltimaAtualizacao { get; set; } = DateTime.Now;
 
     }
